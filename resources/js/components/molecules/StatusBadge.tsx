b4dce1d876995ba180e5d121f2ea4b391af56f7d
bagasrr <bagasramadhan239@gmail.com>
2025-12-01 09:45:22 +0700

    penambahan kategori pada status badge

--- resources/js/components/molecules/StatusBadge.tsx
+++ resources/js/components/molecules/StatusBadge.tsx
@@ -5,12 +5,16 @@ const StatusBadge = ({ status }: { status: string }) => {
         switch (status) {
             case 'Applied':
                 return 'bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200';
-            case 'Skilltest':
+            case 'SkillTest':
                 return 'bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-200';
             case 'Interview':
                 return 'bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-200';
             case 'Offered':
                 return 'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200';
+            case 'Rejected':
+                return 'bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-200';
+            case 'Hired':
+                return 'bg-teal-100 text-teal-800 dark:bg-teal-900 dark:text-teal-200';
             default:
                 // Fallback / Rejected
                 return 'bg-rose-100 text-rose-800 dark:bg-rose-900 dark:text-rose-200';
